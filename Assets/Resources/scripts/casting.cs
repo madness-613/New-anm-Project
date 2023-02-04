09b948e4cfe6cbdf66c9478004e9c25e4991a3fe
madness-613 <austinfaulknerjanes2008@gmail.com>
2023-02-04 10:18:48 -0500

    type update

---
+++ Assets/Resources/scripts/casting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class casting : MonoBehaviour
+{
+  public crystelui cast_1;
+  public crystelui cast_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
